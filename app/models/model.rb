cad16dc25cc53f328febab7b398991bb21db07b4
Austin Smith <trunk9slamchest@gmail.com>
2019-10-08 15:08:23 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,5 +1,21 @@
 class Dog < Sinatra::Base
 
+  attr_reader
+  attr_writer
+  attr_accessor :name, :breed, :age
   
+  @@all = []
+
+	def initialize(name, breed, age)
+	@name = name
+	@breed = breed
+	@age = age
+
+	@@all << self
+	end
+
+	def self.all
+		@@all
+	end  
 
 end
